b55f76c2464548c5d2c993697d9b64d22c7deef6
Nandan-Yadav <nandanyadav.putti@gmail.com>
2025-04-20 22:42:52 +0530

    feat: add global styles, responsive navbar, and chatbot UI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
-function App() {
-  const [count, setCount] = useState(0)
+import { Route, Routes } from "react-router-dom";
+import { ROUTES } from "./Constants/routes_constant";
+import Home from "./pages/Home";
+import LegalInfo from "./pages/LegalInfo";
+import CaseStudiesAndBlogs from "./pages/CaseStudiesAndBlogs";
+import LegalBot from "./pages/LegalBot";
+import UI from "./pages/UI";
+import Navbar from "./components/navbar/Navbar";
+import "./App.css";
+
+export const App = () => {
 
   return (
     <>
-    <h1>Hi</h1>
+      <Navbar />
+      <Routes>
+        <Route path={ROUTES.Home} element={<Home />} />
+        <Route path={ROUTES.LegalInfo} element={<LegalInfo />} />
+        <Route
+          path={ROUTES.CaseStudiesAndBlogs}
+          element={<CaseStudiesAndBlogs />}
+        />
+        <Route path={ROUTES.LegalBot} element={<LegalBot />} />
+        <Route path={ROUTES.UI} element={<UI />} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
